bc941c1fcf5734c75685a62701397df560cc5b1e
nt-mino <m.noto@funda.jp>
2023-04-07 14:47:46 +0900

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,31 @@
+const fetch = require("node-fetch");
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+// Firebaseのデフォルトアプリを初期化する
+admin.initializeApp();
+
+const newRoomWebhookUrl = "ここにウェブフックのURLが入ります";
+
+exports.sendDiscordNotification = functions.firestore
+  .document("messages/{messageId}")
+  .onWrite(async (change, context) => {
+    // データが追加または変更された場合
+    const db = admin.firestore();
+    const messageId = context.params.messageId;
+    const messageDoc = await db.collection("messages").doc(messageId).get();
+    if (!messageDoc.exists) return;
+    const messageData = await messageDoc.data();
+    const message = `新しい通知: ${messageData.content}`;
+
+    await fetch(newRoomWebhookUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        content: `新しい通知: ${message}`,
+      }),
+    });
+  });
